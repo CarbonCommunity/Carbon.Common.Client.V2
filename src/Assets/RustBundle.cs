370c153a9302b1817af3cf6651fcd2ed4d9efae2
raul <raulssorban@gmail.com>
2024-10-13 23:39:42 +0200

    Namespace cleanup

--- src/Assets/RustBundle.cs
+++ src/Assets/RustBundle.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using Facepunch;
 using UnityEngine;
 
 namespace Carbon.Client
